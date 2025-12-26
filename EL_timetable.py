75fd3fae8e52629db6385cf4ef250683ac2790bc
gaiastorage <60290375+gaiastorage@users.noreply.github.com>
2025-12-26 22:18:23 +0800

    Update EL_timetable.py

--- EL_timetable.py
+++ EL_timetable.py
@@ -200,4 +200,5 @@ def add_session():
     students = Student.query.all()
     subjects = Subject.query.all()
     if request.method == "POST":
-        teacher_id = int(request.form.get
+        teacher_id = int(request.form.get("teacher_id"))
+        student_id = int(request.form
